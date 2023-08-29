7f9ee66b9579327fac4669b9d5642d5915535d6c
TwoJungHo <qwer4667@naver.com>
2023-08-30 01:21:35 +0900

    회원가입 ui수정

--- land-information-api/src/components/member/Signup.js
+++ land-information-api/src/components/member/Signup.js
@@ -4,11 +4,27 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
 
 function Signup() {
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [passwordMatch, setPasswordMatch] = useState(false);
+
     const [selectEmail, setSelectEmail] = useState("이메일 선택");
     const [customEmail, setCustomEmail] = useState("");
     const [showCustomInput, setShowCustomInput] = useState(false);
+    const [email, setEmail] = useState(false);
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        setPasswordMatch(event.target.value === password2);
+    };
+
+    const handlePassword2Change = (event) => {
+        setPassword2(event.target.value);
+        setPasswordMatch(event.target.value === password);
+    };
 
     const handleEmailSelect = (email) => {
         if (email === '직접입력') {
@@ -24,25 +40,43 @@ function Signup() {
         setCustomEmail(event.target.value);
     };
 
+    const EmailPush = () => {
+        setEmail(true);
+    }
+
+    const EmailAuth = () => {
+        setEmail(false);
+    }
+
     return (
         <div className='App'>
             <header className="App-header">
                 <img src='/assets/LandMaruBig.png' width={600} height={250}/>
             <InputGroup style={{ width: "500px" }} className="mb-3">
-                    <InputGroup.Text id="basic-addon1" style={{width:"100px"}}>이름</InputGroup.Text>
-                    <Form.Control placeholder='*필수' />
+                    <InputGroup.Text style={{width:"120px"}}>이름</InputGroup.Text>
+                    <Form.Control id="username"  placeholder='*필수' />
                 </InputGroup>
+
                 <InputGroup style={{ width: "500px" }} className="mb-3">
-                    <InputGroup.Text id="basic-addon1" style={{width:"100px"}}>아이디</InputGroup.Text>
-                    <Form.Control placeholder='*필수' />
+                    <InputGroup.Text style={{width:"120px"}}>아이디</InputGroup.Text>
+                    <Form.Control id="id_name" placeholder='*필수' />
                 </InputGroup>
+
                 <InputGroup style={{ width: "500px" }}  className="mb-3">
-                    <InputGroup.Text id="basic-addon1" style={{width:"100px"}} >비밀번호</InputGroup.Text>
-                    <Form.Control placeholder='*필수' type='password'/>
+                    <InputGroup.Text style={{width:"120px"}} >비밀번호</InputGroup.Text>
+                    <Form.Control id="password" placeholder='*필수' type='password' value={password} onChange={handlePasswordChange} />
+                    {passwordMatch ? <FiCheckCircle style={{ color: 'green', marginLeft: '10px' }} /> : <FiXCircle style={{ color: 'red', marginLeft: '10px' }} />}
                 </InputGroup>
+
+                <InputGroup style={{ width: "500px" }}  className="mb-3">
+                    <InputGroup.Text style={{width:"120px"}} >비밀번호확인</InputGroup.Text>
+                    <Form.Control id="password2" placeholder='*필수' type='password' value={password2} onChange={handlePassword2Change} />
+                    {passwordMatch ? <FiCheckCircle style={{ color: 'green', marginLeft: '10px' }} /> : <FiXCircle style={{ color: 'red', marginLeft: '10px' }} />}
+                </InputGroup>
+                
                 <InputGroup style={{ width: "500px" }} className="mb-3">
-                    <InputGroup.Text id="inputGroup-sizing-sm" style={{width:"100px"}}>이메일</InputGroup.Text>
-                    <Form.Control placeholder='*필수'/>
+                    <InputGroup.Text style={{width:"120px"}}>이메일</InputGroup.Text>
+                    <Form.Control id="email" placeholder='*필수'/>
                     {showCustomInput && <Form.Control placeholder="직접 입력" value={customEmail} onChange={handleCustomEmailChange} />}
                     <DropdownButton variant="outline-secondary" title={selectEmail} id="input-group-dropdown-2" align="end">
                         <Dropdown.Item onClick={() => handleEmailSelect('naver.com')}>naver.com</Dropdown.Item>
@@ -54,7 +88,15 @@ function Signup() {
                     
                     
                 </InputGroup>
-                <Button variant="outline-secondary">이메일 발송</Button>
+                {email === false ? (
+                    <Button variant="outline-secondary" onClick={()=> EmailPush()}>이메일 발송</Button>
+                 ):<>
+                 <InputGroup style={{ width: "500px" }} className="mb-3">
+                    <InputGroup.Text id="emailCode" style={{ width: "120px" }}>인증번호</InputGroup.Text>
+                    <Form.Control placeholder='메일로 발송된 인증번호를 적어주세요' />
+                 </InputGroup>
+                 <Button variant="outline-secondary" onClick={() => EmailAuth()}>이메일 인증</Button></>
+                 }
                 <p style={{ fontSize: '12px' }}>E-mail 로 발송된 번호를 확인한 후 인증하셔야 회원가입이 완료됩니다.</p>
 
                 <Button variant="outline-success"style={{width:"500px"}} size="lg">가입 완료</Button>
