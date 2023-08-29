b5d1a292e664ed717717938a459b5a8e43382d7e
TwoJungHo <qwer4667@naver.com>
2023-08-30 01:21:09 +0900

    백엔드 cors에러 수정

--- LandBackend/src/main/java/kr/co/tj/WebMvcConfigImpl.java
+++ LandBackend/src/main/java/kr/co/tj/WebMvcConfigImpl.java
@@ -10,7 +10,7 @@ public class WebMvcConfigImpl implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry regustry) {
 		regustry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://localhost:3000", "http://96junghos.mooo.com:3000")
 		.allowedMethods("GET", "POST", "PUT", "DELETE")
 		.allowedHeaders("*")
 		.allowCredentials(true)
