09d7e0d35632eaa166af8e4f5b56dc5b69d53047
TwoJungHo <qwer4667@naver.com>
2023-08-31 00:12:41 +0900

    cors

--- LandBackend/src/main/java/kr/co/tj/WebMvcConfigImpl.java
+++ LandBackend/src/main/java/kr/co/tj/WebMvcConfigImpl.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfigImpl implements WebMvcConfigurer {
 	
 	@Override
-	public void addCorsMappings(CorsRegistry regustry) {
-		regustry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000", "http://96junghos.mooo.com:3000")
-		.allowedMethods("GET", "POST", "PUT", "DELETE")
-		.allowedHeaders("*")
-		.allowCredentials(true)
-		.maxAge(3600);
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
 	}
 }
