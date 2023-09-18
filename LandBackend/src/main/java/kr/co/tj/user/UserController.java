bf7324bddeb98a21b6ec0786f4623a50d3631477
TwoJungHo <qwer4667@naver.com>
2023-09-18 18:15:02 +0900

    백엔드 유저 코드 간결화

--- LandBackend/src/main/java/kr/co/tj/user/UserController.java
+++ LandBackend/src/main/java/kr/co/tj/user/UserController.java
@@ -1,6 +1,9 @@
 package kr.co.tj.user;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
